cff6081e9cea89d52c1d290420afb717593c63cc
jesses486 <jesses486@gmail.com>
2019-12-02 17:36:23 +0100

    ajout commentaire et javadoc

--- src/main/java/fr/gtm/servlets/AffichageDatesServlet.java
+++ src/main/java/fr/gtm/servlets/AffichageDatesServlet.java
@@ -1,57 +1,57 @@
-package fr.gtm.servlets;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import fr.gtm.entities.DatesVoyages;
-import fr.gtm.entities.Destination;
-import fr.gtm.entities.Image;
-import fr.gtm.services.DestinationServices;
-
+package fr.gtm.servlets;//
+//
+import java.io.IOException;//
+//
+import java.util.ArrayList;//
+import java.util.List;//
+//
+import javax.servlet.RequestDispatcher;//
+import javax.servlet.ServletException;//
+import javax.servlet.annotation.WebServlet;//
+import javax.servlet.http.HttpServlet;//
+import javax.servlet.http.HttpServletRequest;//
+import javax.servlet.http.HttpServletResponse;//
+//
+import fr.gtm.entities.DatesVoyages;//
+import fr.gtm.entities.Destination;//
+import fr.gtm.entities.Image;//
+import fr.gtm.services.DestinationServices;//
+//
 /**
- * Servlet implementation class AffichageDatesServlet
+ * Servlet renvoyant sur une page contenant toutes les destinations
  */
-@WebServlet("/AffichageDatesServlet")
-public class AffichageDatesServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
+@WebServlet("/AffichageDatesServlet")//
+public class AffichageDatesServlet extends HttpServlet {//
+	private static final long serialVersionUID = 1L;//
+//
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DestinationServices service = (DestinationServices) getServletContext().getAttribute(Constantes.DESTINATIONS_SERVICE);
-		Destination destination = new Destination();
-		String ids = request.getParameter("id");
-		String region = request.getParameter("region");
-		long id = Long.parseLong(ids);
-		destination.setId(id);
-		destination.setRegion(region);
-		List<DatesVoyages> datesVoyages =  service.getDatesVoyages(id);
-		String page = "";
-		request.setAttribute("datesVoyages", datesVoyages);
-		request.setAttribute("destination", destination);
-		List<Destination> destinations =  service.getDestinations();
-		request.setAttribute("destinations", destinations);
-		page = "/show-dates.jsp";
-		RequestDispatcher rd = getServletContext().getRequestDispatcher(page);
-		rd.forward(request,response);
-		
-	}
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {//
+		DestinationServices service = (DestinationServices) getServletContext().getAttribute(Constantes.DESTINATIONS_SERVICE);//
+		Destination destination = new Destination();//
+		String ids = request.getParameter("id");//
+		String region = request.getParameter("region");//
+		long id = Long.parseLong(ids);//
+		destination.setId(id);//
+		destination.setRegion(region);//
+		List<DatesVoyages> datesVoyages =  service.getDatesVoyages(id);//
+		String page = "";//
+		request.setAttribute("datesVoyages", datesVoyages);//
+		request.setAttribute("destination", destination);//
+		List<Destination> destinations =  service.getDestinations();//
+		request.setAttribute("destinations", destinations);//
+		page = "/show-dates.jsp";//
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(page);//
+		rd.forward(request,response);//
+//		
+	}//
+//
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
-}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {//
+		doGet(request, response);//
+	}//
+//
+}//
